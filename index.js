3c7648d0072a2f931c4d4e36bd62ced2f34a746c
SuperMighty0 <46725116+SuperMighty0@users.noreply.github.com>
2020-02-19 19:22:03 +0100

    now generating js object for the map

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const Jimp = require('jimp')
 
+const fs = require('fs')
+
 const hexaCodes = {
     '2967666687': 'ocean',
     '8388863': 'forest',
@@ -8,18 +10,30 @@ const hexaCodes = {
 
 const colors = []
 
+let content = 'module.exports = {\n'
+
+
+
 Jimp.read('image1.png', (err, image) => {
     if(err) throw err // ./gqyheuiqgsuehiegshjkesjhkhjegkshjgeshjkgejhkbeshjkgshkgeshkbgjeshgkeghkegkhgekhjeskhjgeskhjhes.psdgq.svg
     // console.log(image.bitmap)
     let pixel = new Array(image.bitmap.width).fill([])
     for(let i= 0; i<image.bitmap.width; i++) {
+        content += `\t${i}: [\n`
         for(let j=0; j< image.bitmap.width; j++) {
-            pixel[i].push({
-                biome: hexaCodes[image.getPixelColor(i, j)]
-            })
+            // pixel[i].push({
+            //     biome: hexaCodes[image.getPixelColor(i, j)]
+            // })
             // console.log(image.getPixelColor(i, j))
+            content += `\t\t{\n\t\t\tbiome: ${hexaCodes[image.getPixelColor(i, j)]}\n\t\t},\n`
         }
+
+        content += '\t],'
     }
 
-    console.log(pixel)
+    content += '}'
+
+    fs.writeFile('./map.js', content, function(err) {
+        if(err) throw err
+    })
 })
